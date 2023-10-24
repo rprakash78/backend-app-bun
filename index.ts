ce650f78faf70d846fce2723ec4430c91172e5a4
rprakash78 <prakashr@lentra.ai>
2023-10-24 21:06:03 +0530

    created a get api to fetch data

---
+++ index.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from "express";
+import restaurants from "./config.json";
+
+const app = express();
+const port = 8080;
+app.use(express.json());
+
+app.get("/detail/:id", (req: Request, res: Response) => {
+  const restaurantId = parseInt(req.params.id);
+
+  // Find the restaurant with the matching ID
+  const restaurant = restaurants.find((r) => r.restaurant_id === restaurantId);
+
+  if (!restaurant) {
+    return res.status(404).json({ error: "Restaurant not found" });
+  }
+
+  // Send the restaurant details as JSON response
+  res.json(restaurant);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
